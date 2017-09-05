c773fb617f0161f469cf98c030061497d7609bd6
RianerJacinto <2014070079@ust-ics.mygbiz.com>
2017-09-05 20:19:35 +0800

    Initial commit

---
+++ app/src/main/java/com/rianer/firstapp/Activity1.java
@@ -0,0 +1,23 @@
+package com.rianer.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+
+    public void NextActivity(View view){
+        Intent intent = new Intent(this,Activity2.class);
+        startActivity(intent);
+
+    }
+
+}
